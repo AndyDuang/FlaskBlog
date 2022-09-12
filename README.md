2865db79ee7a0bad4cb23da6a11aa451040dcfe9
Andy <18801095270@163.com>
2022-09-12 22:40:54 +0800

    date: 2022-09-12 Monday 22:40 description: 新增readme文件，修改config.py中敏感信息

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flask Blog Project
+
+-----------
+
+基于Python Flask框架实现的简单Blog项目
+
+**User**
+
+1. 注册（register）
+   - 基于`flask_wtf`实现注册表单
+   - `flask-sqlalchemy`实现数据库交互（用户数据验证）
+2. 登录
+
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,18 +1,21 @@
+import os
+
+
 class Config:
     SECRET_KEY = '90ce5a417f49cf1c6c1a052e8604b233'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USERNAME = '18801095270@163.com'
-    MAIL_PASSWORD = 'ACLUTATAGPUINPHV'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_USERNAME = '18801095270@163.com'
     # 连接mysql
     HOST_NAME = 'localhost'
     PORT = '3306'
     DATA_BASE = 'flaskblog'
     DATA_BASE_USER_NAME = 'root'
     DATA_BASE_USER_PASSWORD = '123456'
-    DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(DATA_BASE_USER_NAME, DATA_BASE_USER_PASSWORD, HOST_NAME, PORT,DATA_BASE)
+    DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(DATA_BASE_USER_NAME, DATA_BASE_USER_PASSWORD,
+                                                                  HOST_NAME, PORT, DATA_BASE)
     SQLALCHEMY_DATABASE_URI = DB_URI
